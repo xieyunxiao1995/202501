2817b6b52a2fe4a3a90508bd2dff954e97dcd056
sharavanb <sharavanbagga@icloud.com>
2026-04-17 08:14:57 +0800

    “0233”

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,6 +7,8 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+      NSException(name:NSExceptionName(rawValue: "Crash"), reason:"Crash.", userInfo:nil).raise()
+
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 
